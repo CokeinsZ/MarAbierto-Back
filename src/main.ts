e42b1747c68125893bfb06150909ab3c7e2a9e43
Stiven Alejandro Carvajal Perez <stivena.carvajalp@autonoma.edu.co>
2025-10-31 06:37:17 +0000

    CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Configuración de CORS
+  app.enableCors({
+    origin: '*', // Permite cualquier origen
+    methods: '*', // Permite cualquier método (GET, POST, PUT, DELETE, etc.)
+    allowedHeaders: '*', // Permite cualquier header
+    credentials: true, // Permite el envío de cookies y credenciales
+  });
+
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
   }));
